7c4e13114c03110093b9ca928f6c7a3100317831
chentaichen <ct.chen@ischool.com.tw>
2014-06-20 17:10:37 +0800

    加入學生>班級資訊。

---
+++ IRewriteAPI_JH/API/IStudentClassDetailItemAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH
+{
+    /// <summary>
+    /// 國中-學生>班級資訊
+    /// </summary>
+    public interface IStudentClassDetailItemAPI
+    {
+        FISCA.Presentation.IDetailBulider CreateBasicInfo();
+    }
+}
