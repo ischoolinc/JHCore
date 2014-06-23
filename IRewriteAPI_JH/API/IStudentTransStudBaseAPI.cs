5b8257005346c6748de13f135bb6cf42f87b0bb5
chentaichen <ct.chen@ischool.com.tw>
2014-06-23 15:13:06 +0800

    新增教務作業>轉入異動

---
+++ IRewriteAPI_JH/API/IStudentTransStudBaseAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH
+{
+    /// <summary>
+    /// 國中系統 - 轉入異動-轉入學生基本資料
+    /// </summary>
+    public interface IStudentTransStudBaseAPI
+    {
+        FISCA.Presentation.IDetailBulider CreateBasicInfo();
+    }
+}
