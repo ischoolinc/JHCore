c1d312cf8c15b92997ef06bd2e3caf5b770117a2
chentaichen <ct.chen@ischool.com.tw>
2014-07-10 15:44:29 +0800

    加入國中匯入學生基本資料。

---
+++ IRewriteAPI_JH/API/IStudentImportWizardAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH
+{
+    /// <summary>
+    /// 匯入學生基本資料
+    /// </summary>
+    public interface IStudentImportWizardAPI
+    {
+        FISCA.Presentation.Controls.BaseForm CreateForm();
+    }
+}
