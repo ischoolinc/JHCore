77e0d9f77a788024039eede8b1254817b99d96c8
chentaichen <ct.chen@ischool.com.tw>
2014-10-14 16:21:36 +0800

    因高雄自動編班調整-新增學生-加入訊息。

---
+++ IRewriteAPI_JH/API/IStudentAddStudentAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH
+{
+    /// <summary>
+    /// 國中系統 - 學生-新增學生
+    /// </summary>
+    public interface IStudentAddStudentAPI
+    {
+        FISCA.Presentation.Controls.BaseForm CreateForm();
+    }
+}
---
+++ IRewriteAPI_JH/API/IStudentChangeStatusAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH.API
+{
+    public interface IStudentChangeStatusAPI
+    {
+       FISCA.Presentation.IDescriptionPaneBulider CreateBasicInfo();
+    }
+}
