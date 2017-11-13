3cd777eaa724ab3ee9021648a4994b829e0d9543
ED <yinghua.lin@ischool.com.tw>
2017-11-13 13:55:10 +0800

    2017/11/13 穎驊使用均泰建議的方式，使用IRewriteAPI_JH 新增註冊 刪除學生的按鈕 "介面" (其真正實作會在KH_HighConcern 資源檔內)

---
+++ IRewriteAPI_JH/API/IStudentDeleteStudentAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRewriteAPI_JH
+{
+
+    //// 2017/11/9 穎驊註解，因應高雄小組項目 [06-01][03] 修改學生狀態沒有上傳局端 而新增的Code
+
+    /// <summary>
+    /// 國中系統 - 學生-刪除學生
+    /// </summary>
+    public interface IStudentDeleteStudentAPI
+    {
+        FISCA.Presentation.Controls.BaseForm CreateForm();
+    }
+}
